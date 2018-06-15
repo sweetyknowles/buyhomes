24d7d245509fcd57a12e5da6da8d2092bb3b6ed4
SweetyKnowles <sweetyjames@hotmail.co.uk>
2018-06-15 14:33:53 -0400

    schema created

---
+++ db/schema.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const HomeSchema = new Schema({
+title: {
+    type: String,
+    default: 'Home Title'
+},
+
+location: {
+    type: String,
+    default: 'Home Location'
+},
+
+price: {
+    type: Number,
+    default: 'Home Price'
+},
+
+description: {
+    type: String,
+    default: 'Home Description'
+}
+},{
+    timestamps: true
+})
+
+const BuyerSchema = new Schema({
+
+    name: String, 
+    homes: [ HomeSchema ]
+})
+
+module.exports = {
+    BuyerSchema,
+    HomeSchema
+}
